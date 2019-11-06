1fa46e3ee1364ff82e41c47642b715ccc0a19190
hvanderwall <h.e.c.van.der.wall@lacdr.leidenuniv.nl>
2019-11-06 15:36:32 +0100

    initial commit

---
+++ fish_project/fish_project.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-"""Documentation about the Fish project module."""
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+# FIXME: put actual code here
+def example():
+    logger.info('Providing information about the excecution of the function.')
+    print('Hello Fishy Things')
+
+if __name__ == "__main__":
+    example()
